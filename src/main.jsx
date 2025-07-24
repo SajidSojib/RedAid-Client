86fba92bbe708a68fc7b515d995898f062ee36c5
SajidSojib <sajidahmed2401@gmail.com>
2025-07-24 22:53:42 +0600

    besic setup with react router, daisy, tailwind, motion, aos, react-icons and other necessary packages

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { ToastContainer } from "react-toastify";
+import MainLayout from './Layouts/MainLayout';
+import Home from './Pages/Home/Home';
+AOS.init();
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: Home
+      },
+    ]
+  },
+]);
+
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+    <ToastContainer />
+  </StrictMode>
+);
